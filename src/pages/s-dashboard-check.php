23a2351378fccbc98a0e72b99598bfa313efb57e
민정일 <neohum77@gmail.com>
2024-08-24 20:36:16 +0900

    add student test1, test2 003

--- src/pages/s-dashboard-check.php
+++ src/pages/s-dashboard-check.php
@@ -1,5 +1,6 @@
 <?php 
 declare(strict_types=1);
+
 error_reporting(E_ALL&~E_WARNING);
 date_default_timezone_set('Asia/Seoul');
 
@@ -8,15 +9,32 @@ $data = [];
 $data['studentname'] = $_SESSION['studentname'];
 $data['classname'] = $_SESSION['classname'];
 $data['studentnumber'] =$_SESSION['studentnumber'];
-$data['next_student1'] = (int)$_SESSION['studentnumber'] + 1;
-$data['next_student2'] = (int)$_SESSION['studentnumber'] + 2;
+
+$classTotal = $cms->getFcb()->getClassTotal($_SESSION['classname']);
+$value = 0;
+foreach ($classTotal as $key => $value) {
+  $value;
+}
+
+
+if ($value - $data['studentnumber'] == 0) {
+  $data['next_student1'] = 1;
+  $data['next_student2'] = 2;
+} else if ($value - $data['studentnumber'] == 1) {
+  $data['next_student1'] = $data['studentnumber'] + 1;
+  $data['next_student2'] = 1;
+} else {
+  $data['next_student1'] = (int)$_SESSION['studentnumber'] + 1;
+  $data['next_student2'] = (int)$_SESSION['studentnumber'] + 2;
+}
+
 
 
 $data['checklists'] = $cms->getFcb()->getCheckList($data['classname'], $data['studentname']);
 $data['class_total'] = $cms->getFcb()->getClassTotal($data['classname']);
 
 $data['next_student1s'] = $cms->getFcb()->getSubjectInfo($data['classname'], $data['next_student1'], date("Y-m-d"));
-$data['next_student2s'] = $cms->getFcb()->getSubjectInfo($data['classname'], $data['next_student2'], date("Y-m-d"));
+$data['next_student2s'] = $cms->getFcb()->getSubjectInfo2($data['classname'], $data['next_student2'], date("Y-m-d"));
 
 
 
